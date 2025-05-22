f7e0eee6e23ddf0e936998ccb8b8e126761916dc
sf7kmmr <sf7kmmr@gmail.com>
2025-05-22 21:32:28 +0500

    btn min w fix

--- resources/views/front/centrzaim/blocks/components/get-money-btn.blade.php
+++ resources/views/front/centrzaim/blocks/components/get-money-btn.blade.php
@@ -1,4 +1,4 @@
-<div data-next class="money-btn bg-blue text-white text-center sm:w-auto w-full min-w-[313px] h-[60px]
+<div data-next class="money-btn bg-blue text-white text-center sm:w-auto w-full  h-[60px]
                     px-14 sm:py-3 py-3 rounded-2xl cursor-pointer text-lg flex items-center justify-center {{$class ?? ''}}">
     {{$btnText ?? 'Получить деньги'}}
 </div>
