107c2efed80db51b8ff8a12c54babfb20be3cc7f
sfMac <sf7kmmr@gmail.com>
2025-06-08 00:49:38 +0400

    mobile verstka

--- resources/views/front/centrzaim/components/cookie-agree.blade.php
+++ resources/views/front/centrzaim/components/cookie-agree.blade.php
@@ -1,14 +1,29 @@
-<div class="fixed w-[80%] bottom-4 z-50 px-2 left-1/2 transform -translate-x-1/2">
-    <div class="bg-white shadow-lg rounded-xl px-3 py-4 flex  flex-row
-    items-center justify-between gap-4 border border-blue">
-        <img src="/assets/ctr/img/cookie.svg" alt=""/>
-        <p class="text-sm text-gray-700 w-[70%]">
-            Продолжая пользоваться сайтом, вы даёте согласие на использование cookie и обработку персональных данных. Подробнее в
-            <a href="/docs/ctr/Политика_конфиденциальности.pdf" target="_blank" class="text-blue-600 underline">Политике конфиденциальности</a>.
-        </p>
-        <button class="bg-blue shrink-0
-        text-white text-sm font-medium px-4 py-3 rounded-lg hover:bg-blue-700 transition">
-            Спасибо, понятно!
-        </button>
+<div class="fixed container bottom-4 z-50 px-2 left-1/2 transform -translate-x-1/2">
+    <div class="bg-white shadow-lg rounded-xl px-3 py-4 border border-blue">
+        <!-- Десктоп версия -->
+        <div class="hidden md:flex flex-row items-center justify-between gap-4">
+            <img src="/assets/ctr/img/cookie.svg" alt=""/>
+            <p class="text-sm flex-1 opacity-60">
+                Продолжая пользоваться сайтом, вы даёте согласие на использование cookie и обработку персональных данных. Подробнее в
+                <a href="/docs/ctr/Политика_конфиденциальности.pdf" target="_blank" class="text-blue-600 underline">Политике конфиденциальности</a>.
+            </p>
+            <button class="bg-blue shrink-0 text-white text-sm font-medium px-4 py-3 rounded-lg hover:bg-blue-700 transition">
+                Спасибо, понятно!
+            </button>
+        </div>
+
+        <!-- Мобильная версия -->
+        <div class="md:hidden">
+            <div class="flex items-start gap-2 mb-4">
+                <img src="/assets/ctr/img/cookie.svg" alt="" class="w-5 h-5 mt-1 shrink-0"/>
+                <p class="text-xs opacity-60">
+                    Продолжая пользоваться сайтом, вы даёте согласие на использование cookie и обработку персональных данных. Подробнее в
+                    <a href="/docs/ctr/Политика_конфиденциальности.pdf" target="_blank" class="text-blue-600 underline">Политике конфиденциальности</a>.
+                </p>
+            </div>
+            <button class="bg-blue w-full text-white text-xs font-medium px-3 py-2.5 rounded-lg hover:bg-blue-700 transition">
+                Спасибо, понятно!
+            </button>
+        </div>
     </div>
 </div>
